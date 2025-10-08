cc9d1f333b5a949161f9f8401e72c8e2bfa9c444
kbpramod3 <kbpramod3@gmail.com>
2025-10-08 12:31:05 +0530

    neon

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,4 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
-dotenv.config();
+import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -8,8 +6,12 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
-    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT || 'postgres',
+    dialectOptions: {
+      ssl: { require: true },
+    },
+    logging: false,
   }
 );
 
