3f19c9fd1e12e1fb83730047d13d7fc0eab75519
kbpramod3 <kbpramod3@gmail.com>
2025-10-08 12:38:39 +0530

    db

--- src/config/db.js
+++ src/config/db.js
@@ -8,9 +8,6 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT || 'postgres',
-    dialectOptions: {
-      ssl: { require: true },
-    },
     logging: false,
   }
 );
